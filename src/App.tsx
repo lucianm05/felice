f835dc0ff8d458c2debb2f98a79615c3786cb352
lucianm05 <lucianmg05@gmail.com>
2023-02-15 19:29:46 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HelloWorld from "./lib";
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.tsx"),
+      name: "felice",
+      fileName: (format) => `felice.${format}.ts`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [react()],
+});
