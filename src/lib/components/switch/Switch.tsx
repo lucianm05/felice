c34107783e304a9430e2470b31eb63565ada6e88
lucianm05 <lucianmg05@gmail.com>
2023-06-24 12:57:47 +0300

    typing changes; prevent click; forward ref

--- src/lib/components/switch/Switch.tsx
+++ src/lib/components/switch/Switch.tsx
@@ -5,75 +5,80 @@ import {
 } from '@lib/components/switch/types'
 import { getClassNames, getStyles } from '@lib/components/switch/utils'
 import { cn, mergeObjects } from '@lib/utils'
-import { CSSProperties, HTMLProps, useState } from 'react'
+import { HTMLProps, MouseEvent, forwardRef, useState } from 'react'
 
 export interface SwitchProps
-  extends Omit<
-    HTMLProps<HTMLButtonElement>,
-    'role' | 'type' | 'aria-checked' | 'aria-label'
-  > {
+  extends Omit<HTMLProps<HTMLButtonElement>, 'role' | 'type' | 'aria-checked'> {
   label?: string
-  styles?: SwitchStyleable<SwitchStyle<CSSProperties>>
-  classNames?: SwitchStyleable<SwitchClassNames<string>>
+  styles?: SwitchStyleable<SwitchStyle>
+  classNames?: SwitchStyleable<SwitchClassNames>
+  defaultChecked?: boolean
   checked?: boolean
   onCheckedChange?: (value: boolean) => void
 }
 
-/**
- * @todo
- * event prevent onClick
- * defaultChecked
- */
+const Switch = forwardRef<HTMLButtonElement, SwitchProps>(
+  (
+    {
+      children,
+      label,
+      className,
+      classNames,
+      styles,
+      style,
+      defaultChecked = false,
+      checked: externalChecked,
+      onCheckedChange,
+      onClick,
+      ...props
+    },
+    ref
+  ) => {
+    const [internalChecked, setInternalChecked] = useState(
+      typeof externalChecked === 'boolean' ? externalChecked : defaultChecked
+    )
 
-const Switch = ({
-  children,
-  label,
-  className,
-  classNames,
-  styles,
-  style,
-  checked: externalChecked,
-  onCheckedChange,
-  ...props
-}: SwitchProps) => {
-  const [internalChecked, setInternalChecked] = useState(
-    Boolean(externalChecked)
-  )
+    const checked =
+      typeof externalChecked === 'boolean' ? externalChecked : internalChecked
 
-  const checked =
-    typeof externalChecked === 'boolean' ? externalChecked : internalChecked
+    const onClickInternal = (event: MouseEvent<HTMLButtonElement>) => {
+      onClick?.(event)
 
-  const onClickInternal = () => {
-    const newChecked = !checked
+      if (event.isDefaultPrevented()) return
 
-    setInternalChecked(newChecked)
-    onCheckedChange?.(newChecked)
+      setInternalChecked(!checked)
+      onCheckedChange?.(!checked)
+    }
+
+    return (
+      <button
+        {...props}
+        ref={ref}
+        type='button'
+        role='switch'
+        style={mergeObjects(style, getStyles(styles?.switch, checked))}
+        className={cn(className, getClassNames(classNames?.switch, checked))}
+        onClick={onClickInternal}
+        aria-checked={checked}
+        aria-label={label}
+        data-checked={checked}
+      >
+        {!children && (
+          <>
+            <div
+              aria-hidden
+              style={getStyles(styles?.thumb, checked)}
+              className={getClassNames(classNames?.thumb, checked)}
+            />
+          </>
+        )}
+
+        {children}
+      </button>
+    )
   }
+)
 
-  return (
-    <button
-      {...props}
-      type='button'
-      role='switch'
-      style={mergeObjects(style, getStyles(styles?.switch, checked))}
-      className={cn(className, getClassNames(classNames?.switch, checked))}
-      onClick={onClickInternal}
-      aria-checked={checked}
-      aria-label={label}
-    >
-      {!children && (
-        <>
-          <div
-            aria-hidden
-            style={getStyles(styles?.thumb, checked)}
-            className={getClassNames(classNames?.thumb, checked)}
-          />
-        </>
-      )}
-
-      {children}
-    </button>
-  )
-}
+Switch.displayName = 'FeliceSwitch'
 
 export default Switch
--- src/lib/components/switch/types.ts
+++ src/lib/components/switch/types.ts
@@ -1,14 +1,17 @@
-export interface SwitchRelativeStyle<T = unknown> {
+import { CSSProperties } from 'react'
+
+export interface SwitchRelative<T = unknown> {
   checked?: T
   unchecked?: T
 }
-export type SwitchStyle<T = unknown> = T | SwitchRelativeStyle<T>
 
-export interface SwitchRelativeClassNames<T = unknown>
-  extends SwitchRelativeStyle<T> {
-  default?: T
+export type SwitchRelativeStyle = SwitchRelative<CSSProperties>
+export type SwitchStyle = SwitchRelativeStyle | CSSProperties
+
+export interface SwitchRelativeClassNames extends SwitchRelative<string> {
+  default?: string
 }
-export type SwitchClassNames<T = unknown> = T | SwitchRelativeClassNames<T>
+export type SwitchClassNames = SwitchRelativeClassNames | string
 
 export interface SwitchStyleable<T> {
   switch?: T
