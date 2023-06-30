96c7059f45c9747e9b078882f86ba017ff61e820
lucianm05 <lucianmg05@gmail.com>
2023-06-30 22:07:06 +0300

    npm publish setup

---
+++ src/lib/components/checkbox/index.ts
@@ -0,0 +1,21 @@
+import { Checkbox, CheckboxProps } from './Checkbox'
+import {
+  CheckboxClassName,
+  CheckboxIndicator,
+  CheckboxRelativeClassName,
+  CheckboxRelativeIndicator,
+  CheckboxRelativeStyle,
+  CheckboxStyle,
+} from './types'
+
+export type {
+  CheckboxClassName,
+  CheckboxIndicator,
+  CheckboxProps,
+  CheckboxRelativeClassName,
+  CheckboxRelativeIndicator,
+  CheckboxRelativeStyle,
+  CheckboxStyle,
+}
+
+export { Checkbox }
---
+++ src/lib/components/switch/index.ts
@@ -0,0 +1,18 @@
+import { Switch, SwitchProps } from './Switch'
+import {
+  SwitchClassNames,
+  SwitchRelativeClassNames,
+  SwitchRelativeStyle,
+  SwitchStyle,
+  SwitchStyleable,
+} from './types'
+
+export { Switch }
+export type {
+  SwitchClassNames,
+  SwitchProps,
+  SwitchRelativeClassNames,
+  SwitchRelativeStyle,
+  SwitchStyle,
+  SwitchStyleable,
+}
