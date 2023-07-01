e3489869419c24b05a35e7fb2827d22fff629fe3
lucianm05 <lucianmg05@gmail.com>
2023-07-01 11:32:06 +0300

    finished npm publishing config

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/components/accordion'
-export * from './lib/components/checkbox'
-export * from './lib/components/portal'
-export * from './lib/components/select'
-export * from './lib/components/switch'
